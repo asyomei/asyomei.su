bf90d33d32ab4d96cb90f31eed704d6918696b46
asyomei <neko@asyomei.su>
2025-09-06 02:35:12 +0500

    fix: change url for currents

--- src/urls.ts
+++ src/urls.ts
@@ -9,7 +9,7 @@ export const url = {
       website: 'https://github.com/asyomei/asyomei.su',
     },
   },
-  currents: 'https://currentsofficial.com',
+  currents: 'https://musicbrainz.org/artist/b7e442b4-30cd-4bd2-9238-032e74f7807d',
   gochiusa: 'https://gochiusa.com',
   ayaya10h: 'https://youtu.be/D0q0QeQbw9U',
   teidesu: 'https://tei.su',
