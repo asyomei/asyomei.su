08d905feedd06a15bedcfa204fad2e42d0678291
asyomei <neko@asyomei.su>
2025-09-06 01:29:10 +0500

    feat: add info

--- src/urls.ts
+++ src/urls.ts
@@ -1,5 +1,6 @@
 export const url = {
   my: {
+    nameAtJapanese: 'https://miuki.info/2010/07/russkie-imena-na-yaponskom/',
     matrix: 'https://matrix.to/#/@asyomei:matrix.org',
     telegram: 'https://t.me/asyomei',
     github: 'https://github.com/asyomei',
