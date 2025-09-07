612fc3caf76496658b6dac3b2cc4aa6e9cd1c18c
asyomei <neko@asyomei.su>
2025-09-07 16:20:44 +0500

    feat(actions): remove github
    i think framagit will be enough

--- src/backend/actions/index.ts
+++ src/backend/actions/index.ts
@@ -1,22 +1,14 @@
 import type { Action } from './types'
 import { framagit } from './framagit'
-import { github } from './github'
 import { lastfm } from './lastfm'
 import { shikimori } from './shikimori'
 
 const byDate = <T extends { date: Date }>(a: T, b: T) => b.date.getTime() - a.date.getTime()
 
 export async function fetchActions() {
-  const all = await Promise.all([lastfm(), github(), shikimori(), framagit()])
-  let actions = all.filter(x => x != null)
+  const all = await Promise.all([lastfm(), shikimori(), framagit()])
+  const actions: Action[] = all.filter(x => x != null)
   actions.sort(byDate)
 
-  actions = takeNewest(actions, ['github', 'framagit'])
-
   return actions
-}
-
-function takeNewest(actions: Action[], from: string[]): Action[] {
-  const newest = actions.find(x => from.includes(x.service.name))
-  return actions.filter(x => x === newest || !from.includes(x.service.name))
 }
