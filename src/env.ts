08a04a5d741597668e8b813d05f9d9acf00664e5
Vadim A <vadim@asyomei.su>
2025-10-16 22:49:03 +0500

    fix: use dotenv on prod

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,4 @@
-if (import.meta.env.DEV) {
-  // eslint-disable-next-line antfu/no-top-level-await
-  await import('dotenv/config')
-}
+import 'dotenv/config'
 
 export const env = process.env as {
   LASTFM_TOKEN?: string
